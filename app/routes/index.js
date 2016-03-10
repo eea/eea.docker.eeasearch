0162a56289f50d5e95320a6baf1acd72e29afe24
Luca Pisani <luca.pisani@abstract.it>
2016-03-10 18:11:24 +0100

    refs #68879 eeasearch migrated to latest version of eea.searchserver.js

--- app/routes/index.js
+++
@@ -1,8 +0,0 @@
-/*
- * GET home page.
- */
-var searchServer = require('eea-searchserver')
-
-exports.index = function(req, res) {
-    searchServer.EEAFacetFramework.render(req, res, 'index', {disableBreadcrumbs: true});
-};
