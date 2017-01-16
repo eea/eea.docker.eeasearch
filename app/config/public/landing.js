d9304ba5390c0866b86341ff6feb187a25a6c3ca
ichim-david <ichim.david@gmail.com>
2017-01-16 18:31:41 +0200

    Refs #79869 WIP adding values to the tiles when we are on mobile resolution

--- app/config/public/landing.js
+++ app/config/public/landing.js
@@ -1,3 +1,4 @@
+/* globals jQuery, $ */
 $.fn.landingTile = function(settings) {
     this.data("options", settings);
 
@@ -10,22 +11,22 @@ $.fn.landingTile = function(settings) {
     var getValueFromFacet = function(facet, value){
         var retVal = 0;
         var tree = $("[id='" + facet + "']").closest(".facetview_filter").find(".facetview_tree");
-        var visibleValues = tree.find("li").filter(function(idx, el) { return el.style.display !== "none"})
+        var visibleValues = tree.find("li").filter(function(idx, el) { return el.style.display !== "none";});
         if (value === "count"){
             retVal = visibleValues.length;
         }
         if (value === "min"){
-            var values = jQuery.map(visibleValues, function(element) { if (jQuery(element).find(".facet_label_text").text() !== 'undefined') return jQuery(element).find(".facet_label_text").text(); });
+            var values = jQuery.map(visibleValues, function(element) { if (jQuery(element).find(".facet_label_text").text() !== 'undefined') { return jQuery(element).find(".facet_label_text").text();} });
             values.sort();
             retVal = values[0];
         }
         if (value === "max"){
-            var values = jQuery.map(visibleValues, function(element) { if (jQuery(element).find(".facet_label_text").text() !== 'undefined') return jQuery(element).find(".facet_label_text").text(); });
+            var values = jQuery.map(visibleValues, function(element) { if (jQuery(element).find(".facet_label_text").text() !== 'undefined') { return jQuery(element).find(".facet_label_text").text();} });
             values.sort();
             retVal = values[values.length - 1];
         }
         return retVal;
-    }
+    };
 
     var getValueFromResults = function(value){
         var retVal = 0;
@@ -33,13 +34,13 @@ $.fn.landingTile = function(settings) {
             retVal = $(".eea_results_count").text();
         }
         return retVal;
-    }
+    };
 
     this.bind("facet_ready", function (){
         var options = $(this).data("options");
         if (options.values !== undefined){
             for (var i = 0; i < options.values.length; i++){
-                valueSettingsForTile = {"type": "facet", "facet": options.facet}
+                valueSettingsForTile = {"type": "facet", "facet": options.facet};
                 jQuery.extend(valueSettingsForTile, options.values[i]);
                 var value = 0;
                 if (valueSettingsForTile.type === "facet"){
@@ -57,7 +58,7 @@ $.fn.landingTile = function(settings) {
         var options = $(this).data("options");
         if (options.values !== undefined){
             for (var i = 0; i < options.values.length; i++){
-                valueSettingsForTile = {"type": "facet", "facet": options.facet}
+                valueSettingsForTile = {"type": "facet", "facet": options.facet};
                 jQuery.extend(valueSettingsForTile, options.values[i]);
                 if (valueSettingsForTile.type === "results"){
                     if (valueSettingsForTile.method !== undefined){
@@ -72,14 +73,11 @@ $.fn.landingTile = function(settings) {
         var options = $(this).data("options");
         if (options.values !== undefined){
             for (var i = 0; i < options.values.length; i++){
-                valueSettingsForTile = {"type": "facet", "facet": options.facet}
+                valueSettingsForTile = {"type": "facet", "facet": options.facet};
                 jQuery.extend(valueSettingsForTile, options.values[i]);
                 if (valueSettingsForTile.type === "results"){
                     if (valueSettingsForTile.method !== undefined){
                         valueSettingsForTile.method(valueSettingsForTile.value, valueSettingsForTile.name);
-                    }
-                    else {
-
                     }
                 }
             }
@@ -94,7 +92,7 @@ $.fn.landingTile = function(settings) {
             }
         }
     });
-}
+};
 
 function getAllResults(value, name){
     var search_url = $.fn.facetview.options.search_url;
@@ -109,42 +107,42 @@ function getAllResults(value, name){
 }
 
 jQuery(document).ready(function($) {
-    $(".landing_tile .eea_tile.organisations").landingTile(
+    $(".organisations").landingTile(
         {
             type : "simple",
             facet : "organisation",
             values : [{"value":"count", "name":"organisations_count"}]
         }
     );
-    $(".landing_tile .eea_tile.topics").landingTile(
+    $(".topics").landingTile(
         {
             type : "simple",
             facet : "http://www.eea.europa.eu/portal_types#topic",
             values : [{"value":"count", "name":"topics_count"}]
         }
     );
-    $(".landing_tile .eea_tile.content_types").landingTile(
+    $(".content_types").landingTile(
         {
             type : "simple",
             facet : "http://www.w3.org/1999/02/22-rdf-syntax-ns#type",
             values : [{"value":"count", "name":"content_types_count"}]
         }
     );
-    $(".landing_tile .eea_tile.countries").landingTile(
+    $(".countries").landingTile(
         {
             type : "simple",
             facet : "http://purl.org/dc/terms/spatial",
             values : [{"value":"count", "name":"countries_count"}]
         }
     );
-    $(".landing_tile .eea_tile.time_coverage").landingTile(
+    $(".time_coverage").landingTile(
         {
             type : "simple",
             facet : "year",
             values : [{"value":"min", "name":"time_coverage_min"}, {"type":"fixed", "value":"2016", "name":"time_coverage_max"}]
         }
     );
-    $(".landing_tile .eea_tile.available_content").landingTile(
+    $(".available_content").landingTile(
         {
             type : "custom",
             values : [{"value":"count", "facet":"language", "name":"language_count"}, {"type":"results", "value":"count", "name":"documents_count", "method":getAllResults}]
